1d78030a7a314625ee417933b1a5234138221fee
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-05-16 17:03:29 -0300

    feat: upgrade project, add banner end logo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,23 @@
 import { Box, Typography } from "@mui/material";
+import Logo from "../../shared/assets/header.jpeg";
+import Banner from "../../shared/assets/banner.jpeg";
 
 export default function Home() {
   return (
-    <Box>
-      <Typography variant="h1" component="h2" gutterBottom>
-        Home
-      </Typography>
+    <Box
+      display="flex"
+      flexDirection="column"
+      maxWidth="1200px"
+      minWidth="300px"
+      margin="20px auto"
+      gap={2}
+    >
+      <Box margin="0 auto">
+        <img src={Logo} alt="header" width="400px" />
+      </Box>
+      <Box margin="0 auto">
+        <img src={Banner} alt="" width="1200px" />
+      </Box>
     </Box>
   );
 }
