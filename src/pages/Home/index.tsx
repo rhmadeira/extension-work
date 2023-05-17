5be0019a03f94899cc9d788a74b67210d325b610
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-05-16 23:13:37 -0300

    fix: image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -41,8 +41,16 @@ export default function Home() {
           borderRadius: "2px",
         }}
       />
-      <Typography variant="h3">BEM VINDO A NOSSA CASA</Typography>
-      <img src={Fly} alt="header" width="240px" />
+      <Typography
+        variant={mdDown ? "h5" : "h4"}
+        fontWeight="bold"
+        sx={{
+          color: "#8d5412",
+        }}
+      >
+        BEM VINDO A NOSSA CASA
+      </Typography>
+      <img src={Fly} alt="header" width={mdDown ? "140px" : "240"} />
 
       <Box
         component={Paper}
@@ -139,9 +147,13 @@ export default function Home() {
         <Box
           display="flex"
           flexDirection="column"
+          gap={1}
           width={mdDown ? "100%" : "40%"}
         >
-          <Typography sx={{ alignSelf: "center" }} variant="h4">
+          <Typography
+            sx={{ alignSelf: "center" }}
+            variant={mdDown ? "h4" : "h5"}
+          >
             Sobre nós
           </Typography>
           <Typography
@@ -158,17 +170,27 @@ export default function Home() {
             Paraguai, Argentina, Equador, Haiti, Angola, Moçambique, Inglaterra,
             Itália, Portugal, Índia e muitos outros. Sabemos que há ainda muito
             por fazer, mas o Senhor Deus, dono da Terra, tem levado a grei
-            avivalista a assumir sua posição. Na atualidade, a IEAB tem a
-            seguinte configuração: a Convenção Geral, instância máxima da
-            Igreja, na qual todas as leis e diretrizes são estabelecidas, bem
-            como se elege o quadro de liderança superior, o Conselho Geral, que
-            é composto do Presidente, dos Superintendentes Regionais e Diretores
-            Gerais. Temos 4 áreas gerias de atuação: Administração, Ação Social,
-            Cultura e Educação Cristã, Evangelismo e Missões. Trabalhamos com
-            uma divisão territorial no Brasil em 9 Regiões: Sudeste 1, Sudeste
-            2, Sudeste 3, Sul 1, Sul 2, Sul 3, Centro Oeste, Norte e Nordeste;
-            nos demais países a coordenação está sob responsabilidade da área
-            geral de Evangelismo e Missões.
+            avivalista a assumir sua posição.
+          </Typography>
+          <Typography
+            sx={{
+              //type text
+              textAlign: "justify",
+              textJustify: "auto",
+            }}
+            variant="body2"
+          >
+            Na atualidade, a IEAB tem a seguinte configuração: a Convenção
+            Geral, instância máxima da Igreja, na qual todas as leis e
+            diretrizes são estabelecidas, bem como se elege o quadro de
+            liderança superior, o Conselho Geral, que é composto do Presidente,
+            dos Superintendentes Regionais e Diretores Gerais. Temos 4 áreas
+            gerias de atuação: Administração, Ação Social, Cultura e Educação
+            Cristã, Evangelismo e Missões. Trabalhamos com uma divisão
+            territorial no Brasil em 9 Regiões: Sudeste 1, Sudeste 2, Sudeste 3,
+            Sul 1, Sul 2, Sul 3, Centro Oeste, Norte e Nordeste; nos demais
+            países a coordenação está sob responsabilidade da área geral de
+            Evangelismo e Missões.
           </Typography>
           <Typography
             sx={{
